8d3eacfb95aaefc122905eda7b38a9effd8395c0
Stewart Wilson <wilson.x2rt@gmail.com>
2018-05-07 13:13:20 +0200

    Initial commit, env setup, django app, create-react-app, docker setup, ci setup, deploy setup, nginx config

---
+++ scripts/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf /frontend-build/*
+cp -r /frontend/build/* /frontend-build
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+exec /opt/miniconda/envs/app/bin/uwsgi --ini /scripts/uwsgi.ini
