2d303bfe09524e2fb4b4fedc61139e5fa2f29c82
Ilya <shatuncheg@gmail.com>
2018-05-11 14:04:26 +0200

    Fix import in registration serializer, re-revert urls.

--- app/project/api/registration/serializers.py
+++ app/project/api/registration/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.core.mail import EmailMessage
 from rest_framework import serializers
 
-from project.helper_models.models import UserProfile
+from project.user.models import UserProfile
 
 User = get_user_model()
 
--- app/project/api/urls.py
+++ app/project/api/urls.py
@@ -1,7 +1,9 @@
 # from django.urls import path, include
+from django.urls import path, include
 
 app_name = 'api'
 
 urlpatterns = [
-
+    path('auth/', include('project.api.auth.urls', namespace='auth')),
+    path('registration/', include('project.api.registration.urls', namespace='registration')),
 ]
