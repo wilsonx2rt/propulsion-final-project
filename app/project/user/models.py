b786b05180a5367e90a74be7cbf61c120c26e88b
Ilya <shatuncheg@gmail.com>
2018-05-11 13:40:16 +0200

    Create separate app for user.

---
+++ app/project/user/models.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.db import models
+from project.api.helpers import code_generator
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        verbose_name="user",
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="user_profile",
+    )
+    isAdmin = models.BooleanField(
+        verbose_name='User is admin',
+        default=False,
+    )
+    abreviation = models.CharField(
+        verbose_name='User initials',
+        max_length=10,
+        null=True,
+    )
+    code = models.CharField(
+        verbose_name='code',
+        help_text='Random code used for registration and for password reset',
+        max_length=15,
+        default=code_generator
+    )
+
+    def generate_new_code(self):
+        self.code = code_generator()
+        self.save()
+        return self.code
+
+    def __str__(self):
+        return self.user.username
