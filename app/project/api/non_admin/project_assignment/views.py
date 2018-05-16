5686189a8662e5e378449b7947e242a257c31be0
Stewart Wilson <wilson.x2rt@gmail.com>
2018-05-16 10:32:49 +0200

    Changed to retrieve only view on non admin project assignment

--- app/project/api/non_admin/project_assignment/views.py
+++ app/project/api/non_admin/project_assignment/views.py
@@ -1,11 +1,11 @@
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from project.api.project_apps.project_assignment.serializers import ProjectAssignmentSerializer
 from project.project_assignment.models import ProjectAssignment
 
 
-class NonAProjectAssignmentView(RetrieveUpdateAPIView):
+class NonAProjectAssignmentView(RetrieveAPIView):
     '''
     Serves all project assignment fields
     '''
