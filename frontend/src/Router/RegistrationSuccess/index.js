c29a3e91b9de88e72550c0e53d3e45e0e10674ff
Stewart Wilson <wilson.x2rt@gmail.com>
2018-05-21 22:01:25 +0200

    Add login form to registration success page

--- frontend/src/Router/RegistrationSuccess/index.js
+++ frontend/src/Router/RegistrationSuccess/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import LoginForm from '../../Components/LoginForm';
+import { hideNavBar } from '../../helpers';
 
 class RegistrationSuccess extends Component {
+
+  componentDidMount = () => {
+    hideNavBar();
+  };
+
   render() {
     return (
       <div>
         <h1>Success</h1>
+        <div className='login-loginform-container'>
+        <LoginForm />
+      </div>
       </div>
     )
   }
